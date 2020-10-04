6dd04d4192a5bd3dd4940d280a859c1c1574bec9
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2020-10-04 20:43:31 +0900

    feat: !xkcd works some times

--- xkcdump/main.go
+++ xkcdump/main.go
@@ -15,9 +15,13 @@ type xkcdData struct {
 	Alt        string `json:"alt"`
 }
 
+// TODO:
+// Skip 404 in the loop, and write to a single file.
+// 404 is an actual 404 return page, and not the 404th comic!
+
 func main() {
-	data := make([]*xkcdData, 2367-404)
-	for i := 405; i <= 2367; i++ {
+	data := make([]*xkcdData, 404)
+	for i := 1964; i <= 2367; i++ {
 		apiEndpoint := fmt.Sprintf("https://xkcd.com/%d/info.0.json", i)
 		resp, err := http.Get(apiEndpoint)
 		if err != nil {
@@ -28,7 +32,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		data[i-1] = val
+		data[i-1964] = val
 		file, err := json.MarshalIndent(data, "", " ")
 		if err != nil {
 			panic(err)
