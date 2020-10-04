104d4dc4afde3ce3f597569f052e6106bf500966
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2020-10-04 20:43:31 +0900

    feat: !xkcd works some times

--- cmd_xkcd.go
+++ cmd_xkcd.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/gempir/go-twitch-irc/v2"
 )
@@ -24,6 +25,12 @@ func (b *twitchBot) handleXkcd(msg *twitch.PrivateMessage) error {
 		ctx = context.Background()
 		err error
 	)
+
+	args := strings.Split(msg.Message, " ")
+	if len(args) < 2 {
+		b.respond("There isn't an XKCD for that: https://xkcd.com/404")
+	}
+	arg := args[1]
 
 	xkcdDump := []xkcdData{}
 	b.xkcdOnce.Do(func() {
@@ -48,7 +55,43 @@ func (b *twitchBot) handleXkcd(msg *twitch.PrivateMessage) error {
 	if err != nil {
 		return err
 	}
-	comic := xkcdDump[406]
-	b.respond("There's an xkcd for that: " + fmt.Sprintf(linkFormat, comic.Num))
+
+	scores := map[int]int{}
+	for _, comic := range xkcdDump {
+		// title has a weight of 10
+		// Transcript has a weight of 3
+		// alt has a weight of 1
+		wordsTitle, wordsTranscript, wordsAlt := strings.Split(comic.SafeTitle, " "), strings.Split(comic.Transcript, " "), strings.Split(comic.Alt, " ")
+		for _, word := range wordsTitle {
+			if word == arg {
+				scores[comic.Num] += 10
+			}
+		}
+		for _, word := range wordsTranscript {
+			if word == arg {
+				scores[comic.Num] += 3
+			}
+		}
+		for _, word := range wordsAlt {
+			if word == arg {
+				scores[comic.Num] += 1
+			}
+		}
+	}
+
+	max := 404
+	currentMaxScore := 0
+	for num, score := range scores {
+		if score > currentMaxScore {
+			currentMaxScore = score
+			max = num
+		}
+	}
+	if max == 404 {
+		b.respond("There isn't an XKCD for that: https://xkcd.com/404")
+		return nil
+	}
+
+	b.respond("There's an xkcd for that: " + fmt.Sprintf(linkFormat, max))
 	return nil
 }
