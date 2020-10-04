0cfbfbc6b2e836c488b685d0b9e40d06005c31a5
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2020-10-04 20:08:46 +0900

    feat: fix temp issue in xkcdump and flesh out skeleton for !xkcd

--- cmd_xkcd.go
+++ cmd_xkcd.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/gempir/go-twitch-irc/v2"
 )
@@ -16,7 +20,35 @@ type xkcdData struct {
 const linkFormat = "https://xkcd.com/%d/"
 
 func (b *twitchBot) handleXkcd(msg *twitch.PrivateMessage) error {
-	var comic xkcdData
+	var (
+		ctx = context.Background()
+		err error
+	)
+
+	xkcdDump := []xkcdData{}
+	b.xkcdOnce.Do(func() {
+		jsonFile, err := os.Open("./xkcdump.json")
+		if err != nil {
+			err = b.notifier.Wrap(ctx, err, "unable to open xkcdump.json")
+			return
+		}
+		defer jsonFile.Close()
+		byteValue, err := ioutil.ReadAll(jsonFile)
+		if err != nil {
+			err = b.notifier.Wrap(ctx, err, "unable to read xkcdump.json file")
+			return
+		}
+
+		err = json.Unmarshal(byteValue, &xkcdDump)
+		if err != nil {
+			err = b.notifier.Wrap(ctx, err, "unable to read parse xkcdump.json as JSON")
+			return
+		}
+	})
+	if err != nil {
+		return err
+	}
+	comic := xkcdDump[406]
 	b.respond("There's an xkcd for that: " + fmt.Sprintf(linkFormat, comic.Num))
 	return nil
 }
