b36d3765311d83658b4c87d8fde1ebb94198637e
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2020-10-14 12:22:45 +0900

    fix: Actually fill in username :facepalm:

--- cmd_lurk.go
+++ cmd_lurk.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 
 	"github.com/gempir/go-twitch-irc/v2"
 )
 
 func (b *twitchBot) handleLurk(msg *twitch.PrivateMessage) error {
+	un := msg.User.Name
 	lurkResponses := []string{
 		// Taken from Abed's Batman speech from Community.
-		"If %s chats, there can be no party. %s must be out there in the night, staying vigilant.",
-		"Wherever a party needs to be saved, %s is there.",
-		"Wherever there are masks, wherever there’s tomfoolery and joy, %s is there.",
-		"But sometimes %s is not cause they're out in the night, staying vigilant.",
-		"Watching. !lurk-ing. Running. Jumping. Hurtling. Sleeping. No, %s can’t sleep. You sleep. %s is awake. %s doesn't sleep.",
-		"%s doesn't blink. Is %s a bird? No. %s is a bat. %s is Batman. Or are they? Yes, they're Batman.`)",
+		fmt.Sprintf("If %s chats, there can be no party. %s must be out there in the night, staying vigilant.", un, un),
+		fmt.Sprintf("Wherever a party needs to be saved, %s is there.", un),
+		fmt.Sprintf("Wherever there are masks, wherever there’s tomfoolery and joy, %s is there.", un),
+		fmt.Sprintf("But sometimes %s is not cause they're out in the night, staying vigilant.", un),
+		fmt.Sprintf("Watching. !lurk-ing. Running. Jumping. Hurtling. Sleeping. No, %s can’t sleep. You sleep. %s is awake. %s doesn't sleep.", un, un, un),
+		fmt.Sprintf("%s doesn't blink. Is %s a bird? No. %s is a bat. %s is Batman. Or are they? Yes, they're Batman.", un, un, un, un),
 	}
 	b.respond(lurkResponses[rand.Intn(len(lurkResponses))])
 	return nil
